1aeefd58ca7b1d287fd9eaa5bac862a6dcc0146f
youhyun97 <paris972006@gmail.com>
2019-03-05 17:08:50 +0900

    secret_key_final

--- blog/settings.py
+++ blog/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'k&+$0f7hk$t8gy5imc9=@e)&26n*^k&v^g_su$cv7prlt)gavj'
 import os
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'k&+$0f7hk$t8gy5imc9=@e)&26n*^k&v^g_su$cv7prlt)gavj')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
